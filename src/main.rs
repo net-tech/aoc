7082ebed7ae170d6a6427387f351d8fb598b0aab
net-tech- <hello@nettech.dev>
2023-12-02 22:37:01 +0100

    init, but again

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use std::time::Instant;
+
 mod day1;
 
 fn main() {
     let day = std::env::args().nth(1).expect("Please specify the day to run");
+    let part = std::env::args().nth(2).expect("Please specify the part to run");
 
-    match day.as_str() {
-        "1" => day1::main(),
-        _ => panic!("Unknown day: {}", day),
+    println!("---------------{}.{}---------------", day, part);
+
+    match (day.as_str(), part.as_str()) {
+        ("1", "1") => {
+            let start = Instant::now();
+            day1::part1("./data/day1/data.txt".to_string());
+            println!("Time elapsed: {:?}", start.elapsed());
+        }
+        ("1", "2") => {
+            let start = Instant::now();
+            day1::part2("./data/day1/data.txt".to_string());
+            println!("Time elapsed: {:?}", start.elapsed());
+        }
+        _ => println!("Challenge for day {} part {} not found", day, part),
     }
 }
